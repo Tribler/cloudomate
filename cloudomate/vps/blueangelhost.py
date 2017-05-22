e07c48b7f0171d51a38a211c1bd9b9e384a000ef
Mitchell Hoppenbrouwer <mitchell.h.lang@gmail.com>
2017-05-22 12:34:52 +0200

    fixed pep8 issues

--- cloudomate/vps/blueangelhost.py
+++ cloudomate/vps/blueangelhost.py
@@ -9,51 +9,50 @@ class BlueAngelHost(Hoster):
     name = "blueangelhost"
     website = "https://www.blueangelhost.com/"
     required_settings = ["rootpw"]
+    browser = None
 
     def purchase(self, user_settings, vps_option):
         print 'Purchase'
         pass
 
     def options(self):
-        options = start()
+        options = self.start()
         self.configurations = list(options)
         return self.configurations
 
     def __init__(self):
         pass
 
+    def start(self):
+        browser = mechanize.Browser()
+        browser.set_handle_robots(False)
+        browser.addheaders = [('User-agent', 'Firefox')]
 
-def start(self):
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)
-    browser.addheaders = [('User-agent', 'Firefox')]
+        blue_page = browser.open('https://www.blueangelhost.com/openvz-vps/')
+        return self.parse_options(blue_page)
 
-    blue_page = browser.open('https://www.blueangelhost.com/openvz-vps/')
-    return parse_options(blue_page)
+    def parse_options(self, page):
+        soup = BeautifulSoup(page, 'lxml')
+        month = soup.find('div', {'id': 'monthly_price'})
+        details = month.findAll('div', {'class': 'plan_table'})
+        for column in details:
+            yield self.parse_blue_options(column)
 
-
-def parse_options(self, page):
-    soup = BeautifulSoup(page, 'lxml')
-    month = soup.find('div', {'id': 'monthly_price'})
-    details = month.findAll('div', {'class': 'plan_table'})
-    for column in details:
-        yield parse_blue_options(column)
-
-
-def parse_blue_options(self, column):
-    option = VpsOption()
-    option.name = column.find('div', {'class': 'plan_title'}).find('h4').text
-    option.price = column.find('div', {'class': 'plan_price_m'}).text
-    planinfo = column.find('ul', {'class': 'plan_info_list'})
-    info = planinfo.findAll('li')
-    option.cpu = info[0].text.split(":")[1]
-    option.ram = info[1].text.split(":")[1]
-    option.storage = info[2].text.split(":")[1]
-    option.connection = info[3].text.split(":")[1]
-    option.bandwidth = info[4].text.split("h")[1]
-    option.purchase_url = column.find('a')['href']
-    return option
+    @staticmethod
+    def parse_blue_options(column):
+        option = VpsOption()
+        option.name = column.find('div', {'class': 'plan_title'}).find('h4').text
+        option.price = column.find('div', {'class': 'plan_price_m'}).text
+        planinfo = column.find('ul', {'class': 'plan_info_list'})
+        info = planinfo.findAll('li')
+        option.cpu = info[0].text.split(":")[1]
+        option.ram = info[1].text.split(":")[1]
+        option.storage = info[2].text.split(":")[1]
+        option.connection = info[3].text.split(":")[1]
+        option.bandwidth = info[4].text.split("h")[1]
+        option.purchase_url = column.find('a')['href']
+        return option
 
 
 if __name__ == "__main__":
-    start()
+    BlueAngelHost.start()
--- cloudomate/vps/ccihosting.py
+++ cloudomate/vps/ccihosting.py
@@ -9,50 +9,49 @@ class CCIHosting(Hoster):
     name = "ccihosting"
     website = "http://www.ccihosting.com/"
     required_settings = ["rootpw"]
+    browser = None
 
     def purchase(self, user_settings, vps_option):
         print 'Purchase'
         pass
 
     def options(self):
-        options = start()
+        options = self.start()
         self.configurations = list(options)
         return self.configurations
 
     def __init__(self):
         pass
 
+    def start(self):
+        self.browser = mechanize.Browser()
+        self.browser.set_handle_robots(False)
+        self.browser.addheaders = [('User-agent', 'Firefox')]
 
-def start(self):
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)
-    browser.addheaders = [('User-agent', 'Firefox')]
+        cci_page = self.browser.open('http://www.ccihosting.com/vps.php')
+        return self.parse_options(cci_page)
 
-    cci_page = browser.open('http://www.ccihosting.com/vps.php')
-    return parse_options(cci_page)
+    def parse_options(self, page):
+        soup = BeautifulSoup(page, 'lxml')
+        tables = soup.findAll('div', {'class': 'box5'})
+        for column in tables:
+            yield self.parse_cci_options(column)
 
-
-def parse_options(self, page):
-    soup = BeautifulSoup(page, 'lxml')
-    tables = soup.findAll('div', {'class': 'box5'})
-    for column in tables:
-        yield parse_cci_options(column)
-
-
-def parse_cci_options(self, column):
-    option = VpsOption()
-    option.name = column.find('div', {'class': 'boxtitle'}).text.split('S')[1]
-    option.price = column.find('div', {'class': 'PriceTag'}).find('span').text.split('U')[0]
-    planinfo = column.find('ul')
-    info = planinfo.findAll('li')
-    option.cpu = info[1].text.split("CPU")[0] + info[1].text.split("CPU")[1]
-    option.ram = info[2].text.split("R")[0]
-    option.storage = info[3].text.split("S")[0]
-    option.bandwidth = info[4].text.split("Ba")[0]
-    option.connection = info[5].text.split("/")[0]
-    option.purchase_url = column.find('a')['href']
-    return option
+    @staticmethod
+    def parse_cci_options(column):
+        option = VpsOption()
+        option.name = column.find('div', {'class': 'boxtitle'}).text.split('S')[1]
+        option.price = column.find('div', {'class': 'PriceTag'}).find('span').text.split('U')[0]
+        planinfo = column.find('ul')
+        info = planinfo.findAll('li')
+        option.cpu = info[1].text.split("CPU")[0] + info[1].text.split("CPU")[1]
+        option.ram = info[2].text.split("R")[0]
+        option.storage = info[3].text.split("S")[0]
+        option.bandwidth = info[4].text.split("Ba")[0]
+        option.connection = info[5].text.split("/")[0]
+        option.purchase_url = column.find('a')['href']
+        return option
 
 
 if __name__ == "__main__":
-    start()
+    CCIHosting.start()
