ea9e6f31692b77351c9da9df593930c4af9ec7fd
René van den Berg <rjwvandenberg@gmail.com>
2017-05-23 12:26:20 +0200

    Implemented registration up to payment

--- cloudomate/vps/pulseservers.py
+++ cloudomate/vps/pulseservers.py
@@ -5,19 +5,62 @@ from cloudomate.vps.vpsoption import VpsOption
 
 
 class Pulseservers(Hoster):
+    '''
+    PulseServers contains the logic to view hosting configurations and to 
+    purchase servers at Pulseservers.
+    '''
     name = "pulseservers"
     website = "https://pulseservers.com/"
-    required_settings = []
+    required_settings = [
+        'firstname',
+        'lastname',
+        'email',
+        'phonenumber',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'phonenumber',
+        'password',
+        'hostname',
+        'rootpw'
+    ]
 
     def options(self):
-        br = self._create_browser()
+        '''
+        Retrieve hosting options at Pulseservers.
+        :return: A list of hosting options
+        '''
+        options = self.start()
+        self.configurations = list(options)
+        return self.configurations
+
+    def start(self):
+        '''
+        Open browser to hoster website and return parsed options
+        :return: parsed options
+        '''
+        self.browser = self._create_browser()
         response = br.open('https://pulseservers.com/vps-linux.html')
+        return self.parse_options(response)
+
+    def parse_options(self, response):
+        '''
+        Parse options of hosting configurations
+        :param response: Site to be parsed
+        :return: list of configurations
+        '''
         site = soup(response.read(), 'lxml')
         pricingboxes = site.findAll('div', {'class': 'pricing-box'})
         self.configurations = [self._parse_box(box) for box in pricingboxes]
         return self.configurations
 
     def _parse_box(self, box):
+        '''
+        Parse a single hosting configuration
+        :param box: Div containing hosting details
+        :return: VpsOption containing hosting details
+        '''
         details = box.findAll('li')
         return VpsOption(
             name = details[0].h4.text,
@@ -31,28 +74,84 @@ class Pulseservers(Hoster):
         )
 
     def _beautify_cpu(self, cores, speed):
+        '''
+        Format cores and speed to fit cpu column
+        :param cores: cores text
+        :param speed: speed text
+        :return: formatted string
+        '''
         spl = cores.split()
         return '{0}c/{1}t {2}'.format(spl[0], spl[3], speed[:-4])
 
     def purchase(self, user_settings, vps_option):
-        br = self._create_browser()
-        br.open(vps_option.purchase_url)
-        br.select_form(predicate = lambda f: 'id' in f.attrs and f.attrs['id'] == 'orderfrm')
-        #<div id="configproducterror" class="errorbox"></div>
-        br.form['billingcycle'] = ['monthly']
-        br.form['hostname'] = user_settings.get('hostname')
-        br.form['rootpw'] = user_settings.get('rootpw')
-        # OS
-        br.form['configoption[3]'] = ['2']
-        # Location
-        br.form['configoption[9]'] = ['63']
-        br.form.new_control('text', 'ajax', {'name': 'ajax', 'value': 1})
-        br.form.new_control('text', 'a', {'name': 'a', 'value': 'confproduct'})
-        br.form.method = "POST"
-        br.submit()
+        '''
+        Purchase a Pulseserver VPS
+        :param user_settings: settings
+        :param vps_option: server configuration
+        :return: 
+        '''
+        print('Purchase')
+        self.register(user_settings, vps_option)
+        pass
 
-        site = br.response().read()
-        print site
+    def register(self, user_settings, vps_option):
+        '''
+        Register at Pulseservers provider and pay through coinbase
+        :param user_settings: 
+        :param vps_option: 
+        :return: 
+        '''
+        self.browser.open(vps_option.purchase_url)
+        self.browser.select_form(predicate = lambda f: 'id' in f.attrs and f.attrs['id'] == 'orderfrm')
+        self.fill_in_server_form(user_settings)
+        self.browser.submit()
+        self.browser.open('https://pulseservers.com/billing/cart.php?a=view')
+        self.browser.select_form(predicate = lambda f: 'id' in f.attrs and f.attrs['id'] == 'mainfrm')
+        self.browser.fill_in_user_form(user_settings)
+        self.browser.submit()
+
+
+    def fill_in_server_form(self, user_settings):
+        '''
+        Fill in the form with user information
+        :param user_settings: settings
+        :return: 
+        '''
+        # <div id="configproducterror" class="errorbox"></div>
+        self.browser.form['billingcycle'] = ['monthly']
+        self.browser.form['hostname'] = user_settings.get('hostname')
+        self.browser.form['rootpw'] = user_settings.get('rootpw')
+        # OS
+        self.browser.form['configoption[3]'] = ['2']
+        # Location
+        self.browser.form['configoption[9]'] = ['63']
+        self.browser.form.new_control('text', 'ajax', {'name': 'ajax', 'value': 1})
+        self.browser.form.new_control('text', 'a', {'name': 'a', 'value': 'confproduct'})
+        self.browser.form.method = "POST"
+
+    def fill_in_user_form(self, user_settings):
+        '''
+        Fill in form with registration information
+        :param user_settings: user info
+        :return: 
+        '''
+        self.browser.form['firstname'] = user_settings.get("firstname")
+        self.browser.form['lastname'] = user_settings.get("lastname")
+        self.browser.form['email'] = user_settings.get("email")
+        self.browser.form['phonenumber'] = user_settings.get("phonenumber")
+        self.browser.form['companyname'] = user_settings.get("companyname")
+        self.browser.form['address1'] = user_settings.get("address")
+        self.browser.form['city'] = user_settings.get("city")
+        countrycode = user_settings.get("countrycode")
+
+        # State input changes based on country: USA (default) -> Select, Other -> Text
+        self.browser.form['country'] = [countrycode]
+        self.browser.form['state'] = user_settings.get("state")
+        self.browser.form['postcode'] = user_settings.get("zipcode")
+        self.browser.form['password'] = user_settings.get("password")
+        self.browser.form['password2'] = user_settings.get("password")
+        self.browser.form['paymentmethod'] = ['coinbase']
+        self.browser.find_control('accepttos').items[0].selected = True
 
 if __name__ == '__main__':
     Pulseservers.purchase({},Pulseservers().options()[1])
