29da6fa83f955fd9dfa3dc59b9ae6036167603b2
Mitchell Hoppenbrouwer <mitchell.h.lang@gmail.com>
2017-06-14 10:30:10 +0200

    options completed, pause

--- cloudomate/vps/cinfu.py
+++ cloudomate/vps/cinfu.py
@@ -1,3 +1,4 @@
+import itertools
 from bs4 import BeautifulSoup
 
 from cloudomate.gateway import coinbase
@@ -37,9 +38,99 @@ class Cinfu(SolusvmHoster):
     def parse_options(self, page):
         soup = BeautifulSoup(page, 'lxml')
         tables = soup.findAll('table', {'class': 'table1'})
-        print tables[1]
-        table = soup.find('table', {'class': 'table1'})
-        return list(self.parse_german_options(table))
+
+        # table = soup.find('table', {'class': 'table1'})
+        options = list(self.parse_german_options(tables[0]))
+        options = itertools.chain(options, self.parse_bulg_usa_options(tables[1]), self.parse_french_options(tables[2]),
+                                  self.parse_bulg_usa_options(tables[3]), self.parse_dutch_options(tables[4]))
+        return options
+
+    def parse_dutch_options(self, table):
+        info = table.findAll('tr')
+        head = True
+        j = 1
+        names = [""] * 6
+        ram = [""] * 6
+        storage = [""] * 6
+        cpu = [""] * 6
+        price = [""] * 6
+        link = [""] * 6
+        for item in info:
+            if head:
+                self.fill_array(names, 'th', item, False)
+                head = False
+            else:
+                if j == 1:
+                    self.fill_array(ram, 'td', item, False)
+                if j == 2:
+                    self.fill_array(storage, 'td', item, False)
+                if j == 4:
+                    self.fill_array(cpu, 'td', item, False)
+                if j == 9:
+                    self.fill_array(price, 'td', item, False)
+                if j == 13:
+                    self.fill_array(link, 'td', item, True)
+                j = j + 1
+        for k in range(6):
+            yield self.fill_options(names[k], ram[k], storage[k], cpu[k], price[k], link[k])
+
+    def parse_french_options(self, table):
+        info = table.findAll('tr')
+        head = True
+        j = 1
+        names = [""] * 10
+        ram = [""] * 10
+        storage = [""] * 10
+        cpu = [""] * 10
+        price = [""] * 10
+        link = [""] * 10
+        for item in info:
+            if head:
+                self.fill_array(names, 'th', item, False)
+                head = False
+            else:
+                if j == 1:
+                    self.fill_array(ram, 'td', item, False)
+                if j == 2:
+                    self.fill_array(storage, 'td', item, False)
+                if j == 4:
+                    self.fill_array(cpu, 'td', item, False)
+                if j == 9:
+                    self.fill_array(price, 'td', item, False)
+                if j == 13:
+                    self.fill_array(link, 'td', item, True)
+                j = j + 1
+        for k in range(10):
+            yield self.fill_options(names[k], ram[k], storage[k], cpu[k], price[k], link[k])
+
+    def parse_bulg_usa_options(self, table):
+        info = table.findAll('tr')
+        head = True
+        j = 1
+        names = [""] * 8
+        ram = [""] * 8
+        storage = [""] * 8
+        cpu = [""] * 8
+        price = [""] * 8
+        link = [""] * 8
+        for item in info:
+            if head:
+                self.fill_array(names, 'th', item, False)
+                head = False
+            else:
+                if j == 1:
+                    self.fill_array(ram, 'td', item, False)
+                if j == 2:
+                    self.fill_array(storage, 'td', item, False)
+                if j == 4:
+                    self.fill_array(cpu, 'td', item, False)
+                if j == 9:
+                    self.fill_array(price, 'td', item, False)
+                if j == 13:
+                    self.fill_array(link, 'td', item, True)
+                j = j + 1
+        for k in range(8):
+            yield self.fill_options(names[k], ram[k], storage[k], cpu[k], price[k], link[k])
 
     def parse_german_options(self, table):
         info = table.findAll('tr')
@@ -68,10 +159,9 @@ class Cinfu(SolusvmHoster):
                     self.fill_array(link, 'td', item, True)
                 j = j + 1
         for k in range(8):
-            yield self.fill_options(names[k],ram[k],storage[k],cpu[k],price[k],link[k])
+            yield self.fill_options(names[k], ram[k], storage[k], cpu[k], price[k], link[k])
 
-
-    def fill_options(self, names, rams, storages, cpus, prices,links):
+    def fill_options(self, names, rams, storages, cpus, prices, links):
         return VpsOption(
             name=names,
             price=float(prices.split('$')[1]),
@@ -81,7 +171,7 @@ class Cinfu(SolusvmHoster):
             storage=float(storages.split("G")[0]),
             bandwidth='unmetered',
             connection=100,
-            purchase_url= links
+            purchase_url=links
         )
 
     def fill_array(self, array, type, item, link):
@@ -95,7 +185,11 @@ class Cinfu(SolusvmHoster):
             temp = item.findAll(type)
             i = 0
             for name in temp[1:]:
-                array[i] = name.text
+                if name.find('strong'):
+                    ram = name.find('strong')
+                    array[i] = ram.text
+                else:
+                    array[i] = name.text
                 i = i + 1
 
 
