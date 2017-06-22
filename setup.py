00b6aff854492b5bbb775f98359573af1e2ac7b9
Jaap Heijligers <jaap@heijligers.me>
2017-06-22 13:30:01 +0200

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='cloudomate',
 
-    version='0.1.4',
+    version='0.1.5',
 
     description='Automate buying VPS instances with Bitcoin',
     long_description=long_description,
