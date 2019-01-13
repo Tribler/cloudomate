928e8906b2c2f1d38123a1d7a70df4dc7ca211f4
bloodyfool <wesselb94@gmail.com>
2019-01-13 19:02:28 +0100

    Add email parsing to orange website

--- cloudomate/hoster/vps/orangewebsite.py
+++ cloudomate/hoster/vps/orangewebsite.py
@@ -3,19 +3,16 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-import json
-import re
-import sys
 from builtins import round
 from builtins import super
 
 from currency_converter import CurrencyConverter
 from future import standard_library
-from mechanicalsoup.utils import LinkNotFoundError
 
-from cloudomate.gateway.coinpayments import CoinPayments
 from cloudomate.hoster.vps.clientarea import ClientArea
+from cloudomate.gateway.coinpayments import CoinPayments
 from cloudomate.hoster.vps.solusvm_hoster import SolusvmHoster
+from cloudomate.hoster.vps.vps_hoster import VpsConfiguration
 from cloudomate.hoster.vps.vps_hoster import VpsOption
 
 standard_library.install_aliases()
@@ -40,6 +37,10 @@ class OrangeWebsite(SolusvmHoster):
     @staticmethod
     def get_clientarea_url():
         return 'https://secure.orangewebsite.com/clientarea.php'
+
+    @staticmethod
+    def get_email_url():
+        return 'https://secure.orangewebsite.com/viewemail.php'  # + ?id=123456
 
     @staticmethod
     def get_gateway():
@@ -121,6 +122,16 @@ class OrangeWebsite(SolusvmHoster):
 
         self._browser.submit_selected()
 
+    def get_configuration(self):
+        clientarea = self._create_clientarea()
+
+        ip = clientarea.get_ip()
+        password = self._settings.get('server', 'root_password')
+
+        if(password == ""):
+            password = clientarea.get_server_information_from_email()['server_password']
+
+        return VpsConfiguration(ip, password)
 
     @classmethod
     def _parse_openvz_hosting(cls, page):
@@ -134,7 +145,7 @@ class OrangeWebsite(SolusvmHoster):
                 name=option.find('h3').text.strip().replace("Virtual Server - ", ""),
                 storage=list_elements[1].text.strip().split(' ')[0][:-2],
                 cores=list_elements[2].text.strip().split(' ')[0],
-                memory=float(list_elements[0].text.strip().split(' ')[0][:-2])/1024,
+                memory=float(list_elements[0].text.strip().split(' ')[0][:-2]) / 1024,
                 bandwidth=list_elements[3].text.strip().split(' ')[0][:-2],
                 connection=1,
                 price=price_usd,
@@ -149,3 +160,65 @@ class OrangeWebsite(SolusvmHoster):
         self._create_controlpanel()
         return self._controlpanel.get_status()
 
+    def _create_clientarea(self):
+        if self._clientarea is None:
+            self._clientarea = OrangeWebsiteClientArea(self.get_browser(), self.get_clientarea_url(),
+                                                       self.get_email_url(), self._settings)
+        return self._clientarea
+
+
+class OrangeWebsiteClientArea(ClientArea):
+    email_url = None
+
+    def __init__(self, browser, clientarea_url, email_url, user_settings):
+        self.email_url = email_url
+        ClientArea.__init__(self, browser, clientarea_url, user_settings)
+
+    def get_emails(self):
+        """
+        Returns a list of dicts containing email metadata: {id, title}
+        This can be used to further select certains emails to parse
+        """
+        self._browser.open(self._url + "?action=emails")
+        soup = self._browser.get_current_page()
+        extracted = self._extract_emails(soup)
+        return extracted
+
+    def get_server_information_from_email(self):
+        """
+        Returns the parsed server information from email
+        """
+        email_id = self._get_email_id()
+        self._browser.open(self.email_url + '?id=' + email_id)
+        soup = self._browser.get_current_page()
+
+        server_info = {
+            'ip_address': None,
+            'server_user': None,
+            'server_password': None,
+        }
+
+        spans = soup.findAll('span', {'style': 'color: #0000ff;'})
+        server_info['server_user'] = spans[0].find('strong').text
+        server_info['server_password'] = spans[1].find('strong').text
+        server_info['ip_address'] = spans[2].text.strip()
+
+        return server_info
+
+    def _get_email_id(self):
+        for email in self.get_emails():
+            e_id = email['id']
+            title = email['title']
+            if "WELCOME EMAIL" in title:
+                return e_id
+
+    @staticmethod
+    def _extract_emails(soup):
+        table = soup.find('table', {'id': 'tableEmailsList'}).tbody
+        emails = []
+        for row in table.findAll('tr'):
+            emails.append({
+                'id': row['onclick'].split('\'')[1].split('id=')[1],
+                'title': row.findAll('td')[1].text
+            })
+        return emails
